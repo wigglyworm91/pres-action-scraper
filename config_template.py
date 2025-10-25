f9ba35c153293a2f1646781e87a6659606ca89bc
Hayley <wigglyworm91@yahoo.com>
2025-10-25 13:40:04 +1100

    update to config files and some restructuring

--- config_template.py
+++ config_template.py
@@ -2,18 +2,24 @@
 
 # OpenAI key - you need a paid account (at least $5) to get one of these. $5 gets you about 1000 summaries; they're very cheap.
 # To make this key, go to https://platform.openai.com/settings/organization/api-keys
-#OPENAI_API_KEY = "sk-FILL_THIS_IN"
+# If you don't want to use OpenAI, leave this blank (or add an invalid key) and the script will only scrape executive orders without summarizing them.
+OPENAI_API_KEY = "sk-FILL_THIS_IN"
 
 
 
 # Discord webhook URL to post updates
 # Discord > Edit Channel > Integrations > Webhooks > New Webhook
-#WEBHOOK_URLS = []
+WEBHOOK_URLS = []
 
 
 
 # Preamble setting
 PREAMBLE = "Please summarize the content of this recent executive order in under 4000 characters."
 # Default preamble is fine, but this is an example of a custom prompt.
-# You can modify this if you want to personalize your output for things that matter for you.
+# You can modify this if you want to personalise your output for things that matter for you.
 #PREAMBLE = "I am someone who lives in Oregon, works as a sanitation engineer for a private company, and travels internationally often. Please summarize the content of this recent executive order in under 4000 characters."
+
+
+POLL_TIME = 5 * 60 # seconds
+
+OUTPUT_DIR = 'orders'
--- script.py
+++ script.py
@@ -1,4 +1,7 @@
+import types
+import typing
 import requests
+import argparse
 from bs4 import BeautifulSoup
 import time
 from datetime import datetime
@@ -13,21 +16,62 @@ import os.path
 import io
 from dataclasses_json import dataclass_json
 from collections import defaultdict
+import logging
+
+class CustomFormatter(logging.Formatter):
+
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+logger = logging.getLogger('pres-action-scraper')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(CustomFormatter())
+logger.addHandler(ch)
 
 try:
-    import config
+    from config import POLL_TIME, PREAMBLE, WEBHOOK_URLS, OPENAI_API_KEY, OUTPUT_DIR
+    assert isinstance(POLL_TIME, int)
+    assert isinstance(PREAMBLE, str)
+    assert isinstance(WEBHOOK_URLS, list)
+    assert all(isinstance(url, str) for url in WEBHOOK_URLS)
+    assert isinstance(OPENAI_API_KEY, str)
+    assert isinstance(OUTPUT_DIR, str)
 except ImportError:
-    raise Exception('You must copy config_template.py to config.py and then fill it in before running this.')
+    logger.error('You must copy config_template.py to config.py and then fill it in before running this.')
+    raise
+except AssertionError as e:
+    logger.error('Your config.py file is malformed. Please check the types of the variables.')
+    raise
 
+if not os.path.exists(OUTPUT_DIR):
+    logger.info(f'Creating output directory at {OUTPUT_DIR}')
+    os.makedirs(OUTPUT_DIR)
 
-POLL_TIME = 5 * 60 # seconds
+if not WEBHOOK_URLS:
+    logger.warning('No WEBHOOK_URLS specified in config.py; no broadcasts will be sent.')
+
 
 ROOT_URL = 'https://www.whitehouse.gov/presidential-actions/'
 
-OUTPUT_DIR = 'orders'
-
-def curtime() -> str:
-    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 def slugify(url: str) -> str:
     '''
@@ -39,8 +83,6 @@ def slugify(url: str) -> str:
     if url.startswith(ROOT_URL):
         return url[len(ROOT_URL):].replace('/', '-')
 
-def tprint(s: str):
-    print(f'[{curtime()}] {s}')
 
 @dataclass_json
 @dataclass
@@ -54,28 +96,28 @@ class ExecutiveOrder:
 
     def load_text(self, force_reload: bool = False) -> None:
         if self.text is None or force_reload:
+            logger.info(f'Loading text for executive order at {self.url}')
             r = requests.get(self.url)
-            if r.status_code != 200:
-                tprint(repr(r))
-                raise Exception(f'Could not get text of executive order at {eo!r}')
+            r.raise_for_status()
             soup = BeautifulSoup(r.text, features='html.parser')
             self.text = markdownify.MarkdownConverter(strip=['h1']).convert_soup(soup.find('main'))
             page_title = soup.find('h1').text.strip()
+            logger.info(f'Loaded text for executive order {page_title} at {self.url}')
             if page_title:
                 if self.title != page_title:
-                    tprint(f'Title changed? Previous title: "{self.title}", but new page title: "{page_title}"')
+                    logger.info(f'Title changed? Previous title: "{self.title}", but new page title: "{page_title}"')
                 self.title = page_title
 
     def generate_summary(self, force_reload: bool = False) -> None:
         if self.summary is None or force_reload:
             client = openai.OpenAI(
-                api_key=config.OPENAI_API_KEY
+                api_key=OPENAI_API_KEY
             )
             completion = client.chat.completions.create(
                 model='gpt-4o-mini',
                 messages=[
                     {'role': 'developer', 'content': 'You are a helpful assistant.'},
-                    {'role': 'user', 'content': f'{config.PREAMBLE} \n\n{self.text}'},
+                    {'role': 'user', 'content': f'{PREAMBLE} \n\n{self.text}'},
                 ],
             )
             self.summary = completion.choices[0].message.content[:4000]
@@ -89,9 +131,10 @@ class ExecutiveOrder:
             i = 2
             while os.path.exists(fpath):
                 fpath = os.path.join(OUTPUT_DIR, f'{fname}-{i}.json')
+                i += 1
         with open(fpath, 'w') as f:
             f.write(data)
-        tprint(f'Wrote {self} to {fpath}')
+        logger.info(f'Wrote {self.title} to {fpath}')
 
     @classmethod
     def load_from(self, fpath: str) -> Self:
@@ -99,11 +142,9 @@ class ExecutiveOrder:
             data = f.read()
         return ExecutiveOrder.from_json(data)
 
-    def cast_to(self, hook: str):
-        if self.text is None:
-            raise Exception('text is None')
-        if self.summary is None:
-            raise Exception('summary is None')
+    def get_hook_data(self) -> tuple[dict, dict[str, typing.Any]]:
+        if self.text is None and self.summary is None:
+            raise Exception(f'text and summary are None for {self.title}')
         obj = {
             'content': None,
             'embed': {
@@ -128,29 +169,36 @@ class ExecutiveOrder:
         else:
             obj['embed'] = {
                 'title': f'{eo.title}',
-                'description': self.text[:4000],
+                'description': self.text[:3997] + '...',
             }
 
         # include the text as an attachment
         files = {
             'text.md': io.StringIO(self.text)
         }
+        return obj, files
 
-        r = requests.post(hook, data=json.dumps(obj), files=files)
-        if r.status_code == 204:
-            tprint(f'Discord notification successfully sent to {hook}')
-        else:
-            raise Exception(f'Failed to send notification to {hook}. Status code: {r.status_code}')
-
-    def broadcast(self):
+    def broadcast(self, confirm=False) -> None:
+        obj, files = self.get_hook_data()
+        print(f'Broadcasting executive order "{self.title}" to {len(WEBHOOK_URLS)} webhook(s).')
+        logger.info(f'Data to send to Discord webhook:\n{json.dumps(obj, indent=4)} with files {list(files.keys())}')
         for hook in WEBHOOK_URLS:
-            self.cast_to(hook)
+            if confirm:
+                user_confirm = input(f'Send to webhook {hook}? (y/N): ')
+                if user_confirm.lower() != 'y':
+                    logger.info(f'Skipping webhook {hook} per user request')
+                    continue
+            r = requests.post(hook, data={'payload_json': json.dumps(obj)}, files=files)
+            if r.status_code == 204:
+                logger.debug(f'Discord notification successfully sent to {hook}')
+            else:
+                logger.error(f'Failed to send notification to {hook}. Status code: {r.status_code}')
 
 
 def get_current_eos() -> list[ExecutiveOrder]:
     r = requests.get(ROOT_URL)
     if r.status_code != 200:
-        tprint(repr(r))
+        logger.info(repr(r))
         raise Exception('Could not get presidential actions')
     soup = BeautifulSoup(r.text, 'html.parser')
 
@@ -176,43 +224,49 @@ def load_eos_from_disk(direc: str = OUTPUT_DIR) -> list[ExecutiveOrder]:
             eo = ExecutiveOrder.load_from(fname)
             eos.append(eo)
         except Exception as e:
-            tprint(e)
+            logger.info(e)
     return eos
 
 def load_cache_from_disk(direc: str = OUTPUT_DIR) -> dict[str, ExecutiveOrder]:
     eo_cache: dict[str, ExecutiveOrder] = {}
     for eo in load_eos_from_disk():
         if eo.url in eo_cache:
-            tprint(f'Warning: duplicate EO detected: {eo.url} -- should be moved to duplicates folder')
+            logger.info(f'Warning: duplicate EO detected: {eo.url} -- should be moved to duplicates folder')
         eo_cache[eo.url] = eo
     return eo_cache
 
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Process executive orders')
+    parser.add_argument('--once', action='store_true', help='Run once and exit, instead of polling continuously')
+    parser.add_argument('--confirm', action='store_true', help='Ask for confirmation before broadcasting new executive orders')
+    args = parser.parse_args()
+
     eo_cache = load_cache_from_disk()
 
-    first_run = True
+    first_run = False
     while True:
         for eo in get_current_eos():
             if eo.url in eo_cache:
                 # we've already seen this one
-                tprint(f'Skipping {eo} - already seen and broadcast')
+                logger.info(f'Skipping {eo.title} at {eo.url} - already seen')
                 continue
             eo_cache[eo.url] = eo
 
             # this is a new EO
-            tprint(f'Found new EO: {eo.title}\n\t{eo.url}\n\t{eo.date}')
+            logger.info(f'Found new EO: {eo.title}\n\t{eo.url}\n\t{eo.date}')
             try:
                 eo.load_text()
             except Exception as e:
-                tprint(f'Could not load text of EO {eo}: {e}')
+                logger.info(f'Could not load text of EO {eo.title}: {e}')
                 continue
 
             try:
                 eo.generate_summary()
                 pass
             except Exception as e:
-                tprint(f'Could not generate summary of EO {eo}; issue with OpenAI? {e}')
+                logger.warning(f'Could not generate summary of EO {eo.title}; issue with OpenAI? {e}')
                 pass # we don't *need* a summary and openai might just be sad
 
             # no try/catch here - if we can't save to file something is deeply wrong
@@ -222,11 +276,10 @@ if __name__ == '__main__':
                 # we don't actually want to broadcast these
                 continue
 
-            try:
-                eo.broadcast()
-            except Exception as e:
-                tprint(f'Could not broadcast EO {eo}: {e}')
-                continue
+            eo.broadcast(confirm=args.confirm)
+        
+        if args.once:
+            break
 
         first_run = False
         time.sleep(POLL_TIME)
